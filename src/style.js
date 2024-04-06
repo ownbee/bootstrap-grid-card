6da324ab5ef053ebe03e779c4296133ae233ddbe
Alexander Ernfridsson <ernfridsson@gmail.com>
2024-04-06 11:50:46 +0200

    Bring back border and radius on cards
    I think they disappeared in Home Assistant 2024.04 but not entierly sure.
    My theory is that they started disabling border settings on panels in
    this version.

--- src/style.js
+++ src/style.js
@@ -2,6 +2,12 @@ import { css } from 'lit';
 import bootstrapGridStyle from 'bootstrap/dist/css/bootstrap-grid.css';
 
 const style = [bootstrapGridStyle, css`
+    * {
+        // Use home-assistant default (initial = unset) on the following:
+        --ha-card-box-shadow: initial;
+        --ha-card-border-radius: initial;
+        --ha-card-border-width: initial;
+    }
     :host {
         box-sizing: border-box;
     }
